7269630864277bdfe55aeb3535d7a6d1e101badb
TaniQL <taniaquispelazo@gmail.com>
2021-08-30 23:21:07 -0500

    Mock test

--- test/mdlinks.spec.js
+++ test/mdlinks.spec.js
@@ -1,6 +1,11 @@
 const {api} = require("../src/mdlinks.js");
 const route = "/home/tania/LIM015-md-links/filesmd/test.md";
 const routeFalse = "/home/tania/LIM015-md-links/filesmd/tst.md";
+const respuesta = [{href: 'https://github.com/404',
+text: 'Node.js',
+path: '/home/tania/LIM015-md-links/filesmd/testcopy.md',
+status: 404,
+message: 'fail'}];
 
 describe('api', () => {
   it('is a function', () => {
@@ -38,3 +43,25 @@ describe('api', () => {
   })
 });
 
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+it('Return response', () => {
+  fetch.mockResponseOnce(JSON.stringify({
+    result: [
+      {
+        href: 'https://github.com/404',
+        text: 'Node.js',
+        path: '/home/tania/LIM015-md-links/filesmd/testcopy.md',
+        status: 404,
+        message: 'fail'
+      }],
+  }));
+  const res = api('/home/tania/LIM015-md-links/filesmd/testcopy.md',{validate:true})
+  .then((res)=>{
+    expect(res).toEqual(respuesta);
+    }) 
+  //expect(res).toEqual(respuesta);
+  //expect(fetch.mock.calls.length).toEqual(1);
+});
